c36faeac06390d64f380f62bd40aa8b7e557ecf4
katboz <136274505+katboz@users.noreply.github.com>
2025-07-06 18:33:58 +0000

    added main.ts

--- main.ts
+++ main.ts
@@ -21,4 +21,19 @@ console.log(firstName.length);
 
 // My console will write down MURIEL and 7, that is a lenght of the name. 
 
+// any excercise:
 
+let guess;
+guess = 'blue';
+guess = 8;
+// I declared a variable with let named guess, without assigning it a value. Then I assigned color. And at the end I assigned the number. 
+
+// Variable Type Annotations
+
+//This line declares a variable named phoneNumber. It uses TypeScript type annotation : string to tell that phoneNumber must always be a string.
+let phoneNumber : string;
+if (Math.random() > 0.5) {
+  phoneNumber = '+61770102062';
+} else {
+  phoneNumber = '7167762323';
+}
